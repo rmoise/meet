55543ee80997d478b9191569873e15b4ab616d7e
rmoise <roderickfmoise@gmail.com>
2022-12-12 16:00:32 +0100

    add Loading Cached Event Data

--- src/api.js
+++ src/api.js
@@ -72,6 +72,12 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const data = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return data?JSON.parse(data).events:[];;
+  }
+
   const token = await getAccessToken();
 
   if (token) {
@@ -86,6 +92,8 @@ export const getEvents = async () => {
     NProgress.done();
     return result.data.events;
   }
+
+
 };
 
 const getToken = async (code) => {
