f8ed169126e872110e0a07e4b1b3964a4b6a97aa
rmoise <roderickfmoise@gmail.com>
2022-12-12 16:52:07 +0100

    update Loading Cached Event Data

--- src/api.js
+++ src/api.js
@@ -17,29 +17,6 @@ export const extractLocations = (events) => {
   return locations;
 };
 
-export const getAccessToken = async () => {
-    const accessToken = localStorage.getItem('access_token');
-
-    const tokenCheck = accessToken && (await checkToken(accessToken));
-
-  if (!accessToken || tokenCheck.error) {
-    await localStorage.removeItem("access_token");
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get("code");
-    if (!code) {
-      const results = await axios.get(
-        "https://knzfdjzm5a.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
-      );
-      const { authUrl } = results.data;
-      return (window.location.href = authUrl);
-    }
-    return code && getToken(code);
-  }
-  return accessToken;
-
-
-}
-
 export const checkToken = async (accessToken) => {
   const result = await fetch(
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
@@ -64,16 +41,33 @@ const removeQuery = () => {
   }
 };
 
+const getToken = async (code) => {
+  const encodeCode = encodeURIComponent(code);
+  const { access_token } = await fetch(
+    'https://knzfdjzm5a.execute-api.eu-central-1.amazonaws.com/dev/api/token' +
+      '/' +
+      encodeCode
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .catch((error) => error);
+
+  access_token && localStorage.setItem('access_token', access_token);
+
+  return access_token;
+};
+
 export const getEvents = async () => {
   NProgress.start();
 
-  if (window.location.href.startsWith("http://localhost")) {
+  if (window.location.href.startsWith('http://localhost')) {
     NProgress.done();
     return mockData;
   }
 
   if (!navigator.onLine) {
-    const data = localStorage.getItem("lastEvents");
+    const data = localStorage.getItem('lastEvents');
     NProgress.done();
     return data ? JSON.parse(data).events : [];
   }
@@ -82,30 +76,40 @@ export const getEvents = async () => {
 
   if (token) {
     removeQuery();
-    const url = 'https://knzfdjzm5a.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' + '/' + token;
+    const url =
+      'https://knzfdjzm5a.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' +
+      '/' +
+      token;
     const result = await axios.get(url);
     if (result.data) {
       var locations = extractLocations(result.data.events);
-      localStorage.setItem("lastEvents", JSON.stringify(result.data));
-      localStorage.setItem("locations", JSON.stringify(locations));
+      localStorage.setItem('lastEvents', JSON.stringify(result.data));
+      localStorage.setItem('locations', JSON.stringify(locations));
     }
     NProgress.done();
     return result.data.events;
   }
 };
 
-const getToken = async (code) => {
-    try {
-        const encodeCode = encodeURIComponent(code);
+export const getAccessToken = async () => {
+    const accessToken = localStorage.getItem('access_token');
 
-        const response = await fetch( 'https://knzfdjzm5a.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`)
-        }
-        const { access_token } = await response.json();
-        access_token && localStorage.setItem("access_token", access_token);
-        return access_token;
-    } catch(error) {
-        error.json();
+    const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const results = await axios.get(
+        "https://knzfdjzm5a.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+      );
+      const { authUrl } = results.data;
+      return (window.location.href = authUrl);
     }
+    return code && getToken(code);
+  }
+  return accessToken;
+
+
 }
