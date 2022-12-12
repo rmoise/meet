c87bc6d6a6428dfea0ec8e88b6e31d5d6a4f7b14
rmoise <roderickfmoise@gmail.com>
2022-12-12 16:14:43 +0100

    update Loading Cached Event Data

--- src/api.js
+++ src/api.js
@@ -67,13 +67,13 @@ const removeQuery = () => {
 export const getEvents = async () => {
   NProgress.start();
 
-  if (window.location.href.startsWith('http://localhost')) {
+  if (window.location.href.startsWith("http://localhost")) {
     NProgress.done();
     return mockData;
   }
 
   if (!navigator.onLine) {
-    const data = localStorage.getItem('lastEvents');
+    const data = localStorage.getItem("lastEvents");
     NProgress.done();
     return data ? JSON.parse(data).events : [];
   }
@@ -82,15 +82,12 @@ export const getEvents = async () => {
 
   if (token) {
     removeQuery();
-    const url =
-      'https://knzfdjzm5a.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' +
-      '/' +
-      token;
+    const url = 'https://knzfdjzm5a.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' + '/' + token;
     const result = await axios.get(url);
     if (result.data) {
       var locations = extractLocations(result.data.events);
-      localStorage.setItem('lastEvents', JSON.stringify(result.data));
-      localStorage.setItem('locations', JSON.stringify(locations));
+      localStorage.setItem("lastEvents", JSON.stringify(result.data));
+      localStorage.setItem("locations", JSON.stringify(locations));
     }
     NProgress.done();
     return result.data.events;
